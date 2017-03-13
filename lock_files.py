5eb6dd3cb5d5de86d4355bbc535f11363a7ce62d
Joe Linoff <jlinoff@gmail.com>
2017-03-13 10:18:19 -0700

    Update lock_files.py to increment the release revision

--- lock_files.py
+++ lock_files.py
@@ -113,7 +113,7 @@ except ImportError:
 # Module scope variables.
 #
 # ================================================================
-VERSION = '1.0.8'
+VERSION = '1.0.9'
 th_mutex = Lock()  # mutex for thread IO
 th_semaphore = None  # semapthore to limit max active threads
 th_abort = False  # If true, abort all threads
